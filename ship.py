c096be7faeac1f202e88194353f2f9abfd5102af
Dakota Nagy <dnagy6@student.cscc.edu>
2026-07-18 18:14:09 -0400

    P2 - set up ship ro reset and collide with aliens

--- ship.py
+++ ship.py
@@ -20,11 +20,14 @@ class Ship:
         self.image = pygame.transform.scale(self.image, (self.settings.ship_width, self.settings.ship_height))
 
         self.rect = self.image.get_rect()
-        self.rect.midbottom = self.boundaries.midbottom
+        self._center_ship()
         self.moving_right = False
         self.moving_left = False
-        self.x = float(self.rect.x)
         self.arsenal = arsenal
+
+    def _center_ship(self):
+        self.rect.midbottom = self.boundaries.midbottom
+        self.x = float(self.rect.x)
 
     def update(self):
         """Update the ship's position based on movement flags."""
@@ -53,3 +56,10 @@ class Ship:
         """Signal the arsenal to fire a bullet, if possible."""
 
         return self.arsenal.fire_bullet()
+    
+    def check_collisions(self, other_group):
+        if pygame.sprite.spritecollideany(self, other_group):
+            self._center_ship()
+            return True
+        return False
+    
